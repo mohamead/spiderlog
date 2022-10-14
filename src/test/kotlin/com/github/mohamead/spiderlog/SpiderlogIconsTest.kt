2544fec2c4852602f21c54a5eb1af76fa1f8af1c
mohamead <mohammed.adrik@gmail.com>
2022-10-14 10:06:38 +0200

    Update SpiderlogIconsTest

--- src/test/kotlin/com/github/mohamead/spiderlog/SpiderlogIconsTest.kt
+++ src/test/kotlin/com/github/mohamead/spiderlog/SpiderlogIconsTest.kt
@@ -1,14 +1,18 @@
 package com.github.mohamead.spiderlog
 
 import com.github.mohamead.spiderlog.icons.SpiderlogIcons
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class SpiderlogIconsTest {
 
     @Test
     fun toolWindowIconTest() {
-        assertNotNull(SpiderlogIcons.toolWindowIcon)
+        val icon = SpiderlogIcons.toolWindowIcon
+        assertAll(
+            { assertNotNull(icon) }
+        )
     }
 
 }
